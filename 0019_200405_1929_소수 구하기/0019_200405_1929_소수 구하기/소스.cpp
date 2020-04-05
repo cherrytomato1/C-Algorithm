e705df92771aa4bb675d6c6c1a51eb52af2450b2
cherrytomato1 <49678555+cherrytomato1@users.noreply.github.com>
2020-04-06 00:44:31 +0900

    에라머시기의 체- 시간복잡도: O(nloglogn)

--- 0019_200405_1929_소수 구하기/0019_200405_1929_소수 구하기/소스.cpp
+++ 0019_200405_1929_소수 구하기/0019_200405_1929_소수 구하기/소스.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<algorithm>
 #include<vector>
+#include<math.h>
 
 using namespace std;
 
@@ -21,16 +22,12 @@ int main()
 	for (int i = 2; i <= N; i++)
 		s[i] = i;
 
-	for (int i = 2; i <= N; i++)
+	for (int i = 2; i <= sqrt(N); i++)
 	{
 		if (s[i] == 0)
 			continue;
 		for (int j = i * 2; j <= N; j += i)
-		{
-			if (j > N)
-				break;
 			s[j] = 0;
-		}
 	}
 
 	for (int i = M; i<=N; i++)
@@ -39,4 +36,5 @@ int main()
 
 }
 
-//소수 구하기 - 에라토스테네스의 체
+//소수 구하기 - 에라토스테네스의 체 https://hellowoori.tistory.com/36 시간복잡도 http://doocong.com/algorithm/sieve-of-eratosthenes/
+//소수는 루트N의 경우까지 계산하면 된다 (소수가 아닌 수는 루트N이하의 약수를 갖게된다 3*3 = 9(제일 큰 경우))
