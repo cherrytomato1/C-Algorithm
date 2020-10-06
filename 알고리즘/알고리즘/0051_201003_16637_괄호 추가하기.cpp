25960c790681eca5c847af93b3398d3f6ef2a1b6
cherrytomato1 <terec@naver.com>
2020-10-06 23:19:20 +0900

    51번 문제 정의

---
+++ 알고리즘/알고리즘/0051_201003_16637_괄호 추가하기.cpp
@@ -0,0 +1,32 @@
+/*
+
+	주어진 수식에 임의의 괄호를 추가하여 최댓값을 구하여라
+
+	연산자는 +과 *만 사용됨
+
+	주어지는 수는 0~9
+
+	수식은 항상 홀수이며 1<=N<=19임
+
+	3+3*2
+	-> (3+3)*2
+
+
+*/
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<stack>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio;
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	stack<char> stk;
+
+}
