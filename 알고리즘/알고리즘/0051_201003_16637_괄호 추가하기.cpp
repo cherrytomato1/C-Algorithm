07d695dd35a11739a8da9787c65f9d17d0497cd5
cherrytomato1 <terec@naver.com>
2020-10-10 23:47:49 +0900

    후위표기식 변환

--- 알고리즘/알고리즘/0051_201003_16637_괄호 추가하기.cpp
+++ 알고리즘/알고리즘/0051_201003_16637_괄호 추가하기.cpp
@@ -25,15 +25,107 @@
 
 using namespace std;
 
+void f(stack<char>* stk,vector<char>* vctr, char x)
+{
+	switch (x)
+	{
+	case '0' :
+	case '1' :
+	case '2' :
+	case '3' :
+	case '4' :
+	case '5' :
+	case '6' :
+	case '7' :
+	case '8' :
+	case '9' :
+		vctr->push_back(x);
+		break;
+
+	case '+' :
+	case '-' :
+		if (!stk->empty())
+			switch (stk->top())
+			{
+			case '+':
+			case '-':
+			case '*':
+			case '/':
+				vctr->push_back(stk->top());
+				stk->pop();
+
+			case '(':
+				stk->push(x);
+				break;
+
+			default:
+				printf("op push error1");
+				return;
+			}
+		else
+			stk->push(x);
+		break;
+	case '*' :
+	case '/' :
+		if(!stk->empty())
+			switch (stk->top())
+			{
+			case '*':
+			case '/':
+				vctr->push_back(stk->top());
+				stk->pop();
+			case '+':
+			case '-':
+			case '(':
+				stk->push(x);
+				break;
+			default :
+				printf("op push error 2");
+				return;
+			}
+		break;
+	case '(' :
+		stk->push(x);
+		break;
+	case ')' :
+		while (!stk->empty())
+		{
+			vctr->push_back(stk->top());
+			stk->pop();
+		}
+		break;
+
+
+	default :
+		printf("input error\n");
+		return ;
+	}
+}
+
 int main()
 {
 	ios_base::sync_with_stdio;
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	stack<char> stk;
-	string input;
+	stack<char> op_stk;
+	int length;
+	string str;
+	vector<char> postfix;
 
-	cin >> input;
+	cin >> length;
 
+	cin >> str;
+
+	for (int i = 0; i < length; i++)
+		f(&op_stk, &postfix, str[i]);
+
+	while (!op_stk.empty())
+	{
+		postfix.push_back(op_stk.top());
+		op_stk.pop();
+	}
+
+	for (int i = 0; i < postfix.size(); i++)
+		cout << postfix[i];
 }
