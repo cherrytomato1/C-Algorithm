073b8099d56211fcf3c4d3a3e12334f2887beee9
cherrytomato1 <terec@naver.com>
2020-10-13 19:27:07 +0900

    로봇 청소기 클래스

--- 알고리즘/알고리즘/0052_201012_1996_프린터 큐.cpp
+++ 알고리즘/알고리즘/0052_201012_1996_프린터 큐.cpp
@@ -36,51 +36,46 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 	
-	int temp,size,cnt;
-	int arrT, arrT2, arrT3;
+	int temp,size,cnt;					//size = 현재 남은 문서들의 개수, cnt =현재가지의 출력회수
 
 	cin >> T;
 	
 	while (T--)
 	{
-		fill(&arr[0][0], &arr[Nmax-1][1], 0);
+		fill(&arr[0][0], &arr[Nmax-1][1], 0);			//초기화
 		cin >> N;
 		cin >> M;
 		size = N;
 		cnt = 0;
 
-		for (int i = 0; i < N; i++)
+		for (int i = 0; i < N; i++)						//중요도 받기
 		{
 			cin >> temp;
 			arr[i][0] = i;
 			arr[i][1] = temp;
 		}
 
-		while (arr[M][0] != -1)
+		while (arr[M][0] != -1)							//M 문서가 출력될 때까지
 		{
-			for (int i = 0; i < N; i++)
+			for (int i = 0; i < N; i++)					//매 출력 검사
 			{
-				if (arr[i][0] == 0)
+				if (arr[i][0] == 0)						//해당 인덱스를 가진 문서가 출력차례일 경우
 				{
-					for (int j = 0; j < N; j++)
+					for (int j = 0; j < N; j++)			//다른 모든 문서와 중요도 비교
 					{
-						arrT = arr[j][1];
-						arrT2 = arr[i][1];
-						arrT3 = arr[j][0];
-
-						if ((arr[j][1] > arr[i][1]) && (arr[j][0]!=-1))
+						if ((arr[j][1] > arr[i][1]) && (arr[j][0]!=-1))			//중요도 높은 문서가 있고 그 문서가 아직 출력되지 않은 경우
 						{
 							arr[i][0] += size;
 							break;
 						}
-						if (j == N-1)
+						if (j == N-1)					//모든 검사를 끝내고 중요도 초과 문서가 없을경우, 출력
 						{
 							size--;
 							cnt++;
 						}
 					}
 				}
-				if (arr[i][0] != -1)
+				if (arr[i][0] != -1)					//이미 출력된 문서를 제외한 모든 문서의 현재 위치를 --
 					arr[i][0]--;
 			}
 			//cout << arr[M][0] << " ";
