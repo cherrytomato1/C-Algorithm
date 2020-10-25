1a846f9614f55353a1527511f8720ab977f49618
cherrytomato1 <terec@naver.com>
2020-10-25 19:29:49 +0900

    2438, 11720 번 문제 풀이

---
+++ vscodeSource/0056_201025_11720_numSum/source.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    ios_base:: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N,sum = 0;
+    string str;
+
+    cin >> N;
+    cin >> str;
+
+    for(int i =0; i<N; i++)
+        sum += str[i]-48;
+    
+    cout << sum;
+
+    return 0;
+
+    
+}
