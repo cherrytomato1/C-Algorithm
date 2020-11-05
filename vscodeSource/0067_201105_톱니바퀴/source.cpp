12df6d5ed484e654a52bf64ea2f2fc0e2ad5c2bd
cherrytomato1 <terec@naver.com>
2020-11-05 20:41:39 +0900

    톱니 바퀴 풀이

--- vscodeSource/0067_201105_톱니바퀴/source.cpp
+++ vscodeSource/0067_201105_톱니바퀴/source.cpp
@@ -24,15 +24,17 @@ int T[4][8] = {0};
 void roll(int idx, int preIdx, int ord)
 {
     int temp ;
-    //cout << "roll " << idx << " , ord =  " << ord << ", preidx = " << preIdx << "\n";
+    
+    //현재 톱니바퀴와 왼쪽과 오른쪽의 톱니바퀴가 마주보는 극이 다르면서 비교하는 톱니들이 이미 영향을 준 톱니가 아닐경우
+    //그 톱니바퀴도 돌리지만 영향을 준 현재의 톱니바퀴의 인덱스를 넘겨주어 다시 비교하지 않게함
 
-    if(idx != 0 && T[idx][6] != T[idx - 1][2] && preIdx != idx - 1)
+    if(idx != 0 && T[idx][6] != T[idx - 1][2] && preIdx != idx - 1)       
         roll(idx - 1, idx, ord * -1);
     if(idx != 3 && T[idx][2] != T[idx + 1][6] && preIdx != idx + 1)
         roll(idx + 1, idx, ord * -1);
 
-    //cout << "starting roll..." << "\n";
 
+    // 톱니 돌리기
     if(ord == -1)
     {
         temp = T[idx][0];
@@ -47,24 +49,10 @@ void roll(int idx, int preIdx, int ord)
             T[idx][i] = T[idx][i-1];
         T[idx][0] = temp;
     }
-    //cout << "fin roll : " << idx << ", ord = " << ord << "\n";
 
 }
-
-void check(int idx)
-{
-
-}
-
 int main()
 {
-    /*
-    ios_base :: sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    */
-
-
     int K, idx, ord, score = 0;
 
     for(int i = 0 ; i < 4 ; i++)
@@ -77,20 +65,12 @@ int main()
         cin >> idx;
         cin >> ord;
 
-        roll(idx - 1, -1, ord);       
-        /*
-        for(int i = 0 ; i < 4 ; i++)
-        {
-            for(int j = 0 ; j < 8 ; j++)
-                cout << T[i][j];
-            cout << "\n";
-        }
-        */
+        roll(idx - 1, -1, ord);       //preIdx를 처음엔 -1로 함
     }
 
     for(int i = 0 ; i < 4 ; i++)
         if(T[i][0] == 1)
-            score += pow(2, i);
+            score += pow(2, i);     //점수 계산
 
     cout << score;
 
