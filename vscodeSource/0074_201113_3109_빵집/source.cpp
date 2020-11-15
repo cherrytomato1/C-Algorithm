60c5ba7c80cfe6a9fd4554ffd7867eaeecade6a3
cherrytomato1 <terec@naver.com>
2020-11-15 17:39:12 +0900

    [comment]3109_빵집

--- vscodeSource/0074_201113_3109_빵집/source.cpp
+++ vscodeSource/0074_201113_3109_빵집/source.cpp
@@ -1,9 +1,31 @@
+/*
+
+
+    처음에 틀린 이유 : 모든 row와 row +1을 비교해가면서 최소 접점의 개수를 구하려고 했음
+                    -> 따라서 매 row를 비교하기 위헤 재할당을 반복하여 메모리 초과 혹은 시간 초과 에러가 발생했음
+
+    풀이 : 
+
+    row = 0 부터 시작하여 가능한 경로의 수를 구함.
+    이동시킬 때 이미 지나간 자리를 표시한다. 
+    -> 가능한 경로일 경우 이미 그 경로를 지나가면서 사용하여 다시 사용할 수 없음
+    -> 불가능한 경로일 경우 그 자리를 다시 지나가도 가능한 경로가 없음
+
+    -> 오른쪽 위, 오른쪽, 오른쪽 아래 순으로 시행하여 가능한 위로 이동할 수 있는 경로를 찾는다.
+    col = C-1 까지 이동할 수 있으면 true 반환, 모든 row에서 반복하여 true의 개수를 출력한다. 
+
+
+
+*/
+
+
+
+
+
+
 #include <iostream>
 #include <string>
 #include <vector>
-
-#define rMAX 10000
-#define cMAX 500
 
 using namespace std;
 
@@ -23,14 +45,12 @@ void getMap()
 
 int solve(int row, int col)
 {
-    bool answer = false;
+    map[row][col] = '-';                            //이미 이동한 경로 표시
 
-    map[row][col] = '-';
-
-    if(col == C-1)
+    if(col == C-1)                                  //도착하면 true 반환
         return true;
 
-    if(row != 0 && map[row - 1][col + 1] == '.')
+    if(row != 0 && map[row - 1][col + 1] == '.')    //다음 좌표로 이동하고 그 좌표애서 true 반환시 true 반환, 최종 도착지에서 true를 받아야 true를 받을 수 있음
         if(solve(row - 1, col + 1))
             return true;
 
@@ -42,7 +62,7 @@ int solve(int row, int col)
         if(solve(row + 1, col + 1))
             return true;
 
-    return false;
+    return false;                                    //오른쪽 위, 오른쪽, 오른쪽 아래까지 모두 가볼 수 있는 경로를 지났으나, true인 경우의 수가 없으면 false 반환
     
 }
 
